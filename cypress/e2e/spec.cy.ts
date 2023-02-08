0ecab8c7ce335f4df810e3b66868a146eeffdf9b
Max Edlund <maxedlund88@gmail.com>
2023-02-08 18:55:15 +0100

    add initial rendering tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,20 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(false)
+beforeEach( () => {
+  cy.visit('/');
+})
+
+describe('landing page renders correctly', () => {
+  it('displays empty input field', () => {
+    cy.get('input#searchText').should('exist');
+    cy.get('input#searchText').should('contain', '');
+  }) 
+
+  it('should display correct input placeholder text', () => {
+    cy.get('input#searchText[placeholder="Skriv titel här"]').should('exist');
   })
+
+  it('has search button with correct text', () => {
+    cy.get('button').contains('Sök').should('exist');
+  })
+
+
 })
