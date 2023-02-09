97c3fd8b07bdd822ef3b0b7e2fb9a0ead236fa42
Max Edlund <maxedlund88@gmail.com>
2023-02-09 10:50:26 +0100

    add test with API call

---
+++ cypress/e2e/apiRequest.cy.ts
@@ -0,0 +1,12 @@
+describe('test actual API requests', () => {
+
+  it('should display search result correctly', () => {
+    cy.visit('/');
+    cy.get('input#searchText').type('the phantom menace').should('have.value', 'the phantom menace');
+    cy.get('button').click();
+    cy.get('.movie').should('be.greaterThan', 0);
+    cy.get('#movie-container .movie h3').contains('Star Wars: Episode I - The Phantom Menace').should('exist');
+    
+  })
+
+})
